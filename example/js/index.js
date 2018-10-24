252210d58d6a6d52f5568cc12221d03285d23dff
Wildhoney <adam.timberlake@gmail.com>
2018-10-24 20:52:51 +0100

    Updated Prettier

--- example/js/index.js
+++ example/js/index.js
@@ -1,7 +1,7 @@
 import ꓽ from '/vendor/index.js';
 
-const sayHello = ꓽ`forall a b. String s, Int i => a -> a`(
-    name => `Hello ${name}!`
-);
+const sayHello =
+    ꓽ `forall a b. String s, Int i => a -> a`
+    (name => `Hello ${name}!`);
 
 console.log(sayHello('Adam'));
