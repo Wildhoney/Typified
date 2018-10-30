93677c2372e9987f4e51cd3f3b211bdbca365ed5
Wildhoney <adam.timberlake@gmail.com>
2018-10-30 21:31:16 +0000

    Added simple array scalar validator

--- src/scalars/array/index.js
+++ src/scalars/array/index.js
@@ -1,4 +1,12 @@
-export default function handleArray(scalarType, ast, parameter, generics) {
-    console.log(parameter, scalarType);
-    return true;
+import * as parser from '../../parser/index.js';
+import validate from '../../validator/index.js';
+
+export default function handleArray(scalarType, parameter, generics) {
+    const initial = { generics };
+    const ast = parser.splitTypeDeclaration(scalarType.description);
+    const result = parameter.reduce(
+        (accum, parameter) => validate(ast, scalarType.description, [parameter], accum.generic),
+        initial
+    );
+    return { valid: result.errors.length === 0, generics: result.generics };
 }
--- src/validator/index.js
+++ src/validator/index.js
@@ -5,31 +5,50 @@ import * as u from './utils.js';
 export default function validateDeclaration(ast, declaration, parameters, generics = {}) {
     const initial = { generics, errors: [] };
 
-    return parameters.reduce((accum, parameter, index) => {
+    return [].concat(parameters).reduce((accum, parameter, index) => {
         // Handle the processing of the types.
         const actualType = u.getParameterType(parameter);
         const expectedType = [].concat(accum.generics[ast.types[index]] || ast.types[index]);
         const genericType = expectedType.find(type => ast.generics.includes(type));
-        const matchedType = expectedType.find(type => {
-            const scalarType = parserUtils.maybeParseScalar(type);
-            return scalarType ? validateScalar(scalarType, ast, parameter, accum.generics) : type === actualType;
-        });
+
+        // Validate all of the scalars and primitives.
+        const validationResult = findMatchedType(expectedType, parameter, accum, actualType);
+        const matchedType = validationResult && validationResult.matchedType;
+        const newGenerics = validationResult ? validationResult.generics : [];
 
         // Ensure the type is valid and/or a generic type.
         const isTypeValid = Boolean(matchedType || genericType);
         const isGenericType = Boolean(genericType);
 
         // Setup the return for the `reduce` function.
-        const generics = !isGenericType ? accum.generics : { ...accum.generics, [genericType]: actualType };
-        const errors = isTypeValid
+        const updatedGenerics = {
+            ...newGenerics,
+            ...(!isGenericType ? accum.generics : { ...accum.generics, [genericType]: actualType })
+        };
+        const updatedErrors = isTypeValid
             ? accum.errors
             : [...accum.errors, u.formatTypeMismatchMessage(actualType, expectedType, declaration)];
 
-        return { ...accum, generics, errors };
+        return { ...accum, generics: updatedGenerics, errors: updatedErrors };
     }, initial);
+}
+
+function findMatchedType(expectedType, parameter, accum, actualType) {
+    return expectedType
+        .map(type => {
+            const scalarType = parserUtils.maybeParseScalar(type);
+            return scalarType
+                ? validateScalar(scalarType, parameter, accum.generics)
+                : validatePrimitive(type, actualType);
+        })
+        .find(result => result.valid);
+}
+
+function validatePrimitive(type, actualType) {
+    return { matchedType: type, valid: type === actualType, generics: [] };
 }
 
 function validateScalar(scalarType, ...args) {
     const parseScalar = scalarValidators[scalarType.type] || (() => false);
-    return parseScalar(scalarType, ...args);
+    return { matchedType: `${scalarType.type}(${scalarType.description})`, ...parseScalar(scalarType, ...args) };
 }
