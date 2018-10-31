1217621e5ab8354986c7d235fb5babbf34e1b6a0
Wildhoney <adam.timberlake@gmail.com>
2018-10-31 21:21:54 +0000

    Added function scalar validator placeholder

--- src/scalars/index.js
+++ src/scalars/index.js
@@ -1,7 +1,9 @@
 import handleArray from './array/index.js';
+import handleFunction from './function/index.js';
 
 const defaults = {
-    Array: handleArray
+    Array: handleArray,
+    ƒ: handleFunction
 };
 
 export default new Map(Object.entries(defaults));
