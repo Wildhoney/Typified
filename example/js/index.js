371fb71e952fb708460b249ceb689bbe54b9472d
Wildhoney <adam.timberlake@gmail.com>
2018-11-17 21:37:50 +0000

    Using mathematical arrows in example

--- example/js/index.js
+++ example/js/index.js
@@ -1,11 +1,11 @@
 import ꓽ from '/vendor/index.js';
 
 const getHello =
-    ꓽ `String -> Number -> String`
+    ꓽ `String → Number → String`
     ((name, age) => `${name}! Goodness me you are ${age}!`);
 
 const printAdam =
-    ꓽ `(String -> Number|String -> String) -> void`
+    ꓽ `(String → Number|String → String) → void`
     (helloFn => console.log(helloFn('Adam', 33)));
 
 printAdam(getHello);
