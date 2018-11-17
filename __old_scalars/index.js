727ebfeb9f53b081cdcf64c4037dbd852998055d
Wildhoney <adam.timberlake@gmail.com>
2018-11-17 16:46:12 +0000

    Removed old scalar validators

--- __old_scalars/index.js
+++
@@ -1,9 +0,0 @@
-import handleArray from './array/index.js';
-import handleObject from './object/index.js';
-
-export default new Map(
-    Object.entries({
-        Array: handleArray,
-        Object: handleObject
-    })
-);
