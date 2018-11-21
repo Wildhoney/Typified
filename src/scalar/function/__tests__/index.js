c847b509f351a0b639869c46e6de5257fc26b5d6
Wildhoney <adam.timberlake@gmail.com>
2018-11-21 23:09:39 +0000

    Added a handful of tests for standard direction generic matching

--- src/scalar/function/__tests__/index.js
+++ src/scalar/function/__tests__/index.js
@@ -68,3 +68,28 @@ test('It should be able to validate declarations with alias function types;', t 
         error: `Expected (d -> n -> String) in \`${declaration}\` declaration but received (String s => s -> Number -> s).`
     });
 });
+
+test('It should be able to validate declarations with standard generic function types;', t => {
+    const sayHello = type`String -> Number -> String`((name, age) => `Hello ${name}! You are ${age}.`);
+    const declaration = 'forall a b c. (a -> Number -> a) -> (a -> b -> c) -> (a -> a -> a)';
+    const ast = parser.splitTypeDeclaration(declaration);
+    const validate = createValidator(ast, declaration);
+    t.deepEqual(validate(ast.types[0], sayHello), {
+        valid: true,
+        type: '(a -> Number -> a)',
+        generics: {},
+        error: null
+    });
+    t.deepEqual(validate(ast.types[1], sayHello), {
+        valid: true,
+        type: '(a -> b -> c)',
+        generics: {},
+        error: null
+    });
+    t.deepEqual(validate(ast.types[2], sayHello), {
+        valid: false,
+        type: 'Function',
+        generics: {},
+        error: `Expected (a -> a -> a) in \`${declaration}\` declaration but received (String -> Number -> String).`
+    });
+});
