37f6c90cd80e14a712062c11f2067e4b13c15f94
Wildhoney <adam.timberlake@gmail.com>
2018-11-21 22:44:11 +0000

    Added additional test

--- src/scalar/function/__tests__/index.js
+++ src/scalar/function/__tests__/index.js
@@ -39,14 +39,20 @@ test('It should be able to validate declarations with concrete function types;',
 
 test('It should be able to validate declarations with alias function types;', t => {
     const sayHello = type`String s => s -> Number -> s`((name, age) => `Hello ${name}! You are ${age}.`);
-    const declaration = 'Number n => (String -> n -> String)';
+    const declaration = 'Number n, Date d => (String -> n -> String) -> (d -> n -> String)';
     const ast = parser.splitTypeDeclaration(declaration);
     const validate = createValidator(ast, declaration);
-    const expectedTypes = ast.types[0];
-    t.deepEqual(validate(expectedTypes, sayHello), {
+    t.deepEqual(validate(ast.types[0], sayHello), {
         valid: true,
         type: '(String -> n -> String)',
         generics: {},
         error: null
+    });
+    t.deepEqual(validate(ast.types[1], sayHello), {
+        valid: false,
+        type: 'Function',
+        generics: {},
+        error:
+            'Expected (d -> n -> String) in `Number n, Date d => (String -> n -> String) -> (d -> n -> String)` declaration but received (String s => s -> Number -> s).'
     });
 });
