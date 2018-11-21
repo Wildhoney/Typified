d26d7896c0ec01bbf83f1a0cf76f76c10ebbe86c
Wildhoney <adam.timberlake@gmail.com>
2018-11-21 22:59:18 +0000

    Added more tests for function types

--- src/scalar/function/__tests__/index.js
+++ src/scalar/function/__tests__/index.js
@@ -19,7 +19,8 @@ test('It should be able to validate function types without a declaration;', t =>
 
 test('It should be able to validate declarations with concrete function types;', t => {
     const sayHello = type`String -> Number -> String`((name, age) => `Hello ${name}! You are ${age}.`);
-    const declaration = '(String -> Number -> String) -> (String -> String -> String)';
+    const declaration =
+        '(String -> Number -> String) -> (String|Date -> Boolean|Number -> String) -> (String -> String -> String)';
     const ast = parser.splitTypeDeclaration(declaration);
     const validate = createValidator(ast, declaration);
     t.deepEqual(validate(ast.types[0], sayHello), {
@@ -29,17 +30,23 @@ test('It should be able to validate declarations with concrete function types;',
         error: null
     });
     t.deepEqual(validate(ast.types[1], sayHello), {
+        valid: true,
+        type: '(String|Date -> Boolean|Number -> String)',
+        generics: {},
+        error: null
+    });
+    t.deepEqual(validate(ast.types[2], sayHello), {
         valid: false,
         type: 'Function',
         generics: {},
-        error:
-            'Expected (String -> String -> String) in `(String -> Number -> String) -> (String -> String -> String)` declaration but received (String -> Number -> String).'
+        error: `Expected (String -> String -> String) in \`${declaration}\` declaration but received (String -> Number -> String).`
     });
 });
 
 test('It should be able to validate declarations with alias function types;', t => {
     const sayHello = type`String s => s -> Number -> s`((name, age) => `Hello ${name}! You are ${age}.`);
-    const declaration = 'Number n, Date d => (String -> n -> String) -> (d -> n -> String)';
+    const declaration =
+        'Number n, Date d => (String -> n -> String) -> (String -> n|d -> String|Boolean) -> (d -> n -> String)';
     const ast = parser.splitTypeDeclaration(declaration);
     const validate = createValidator(ast, declaration);
     t.deepEqual(validate(ast.types[0], sayHello), {
@@ -49,10 +56,15 @@ test('It should be able to validate declarations with alias function types;', t 
         error: null
     });
     t.deepEqual(validate(ast.types[1], sayHello), {
+        valid: true,
+        type: '(String -> n|d -> String|Boolean)',
+        generics: {},
+        error: null
+    });
+    t.deepEqual(validate(ast.types[2], sayHello), {
         valid: false,
         type: 'Function',
         generics: {},
-        error:
-            'Expected (d -> n -> String) in `Number n, Date d => (String -> n -> String) -> (d -> n -> String)` declaration but received (String s => s -> Number -> s).'
+        error: `Expected (d -> n -> String) in \`${declaration}\` declaration but received (String s => s -> Number -> s).`
     });
 });
