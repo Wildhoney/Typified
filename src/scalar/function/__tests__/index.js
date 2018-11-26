d5e8ad6bc675cde977bb5ff1bb9b1091b853875b
Wildhoney <adam.timberlake@gmail.com>
2018-11-26 08:13:40 +0000

    Removed logic for generics

--- src/scalar/function/__tests__/index.js
+++ src/scalar/function/__tests__/index.js
@@ -98,7 +98,7 @@ test('It should be able to validate declarations with standard generic function 
 
 test('It should be able to validate declarations with reversed generic function types;', t => {
     const sayHello = type`forall a b. a -> b -> a`((name, age) => `Hello ${name}! You are ${age}.`);
-    const declaration = 'forall s n. (s -> n -> s) -> (String -> Number -> String) -> (s -> s -> s)';
+    const declaration = 'forall s n. (s -> n -> s) -> (String -> Number -> String) -> (s -> s -> s) -> (s -> n -> n)';
     const ast = parser.splitTypeDeclaration(declaration);
     const validate = createValidator(ast, declaration);
     t.deepEqual(validate(ast.types[0], sayHello), {
@@ -114,10 +114,16 @@ test('It should be able to validate declarations with reversed generic function 
         error: null
     });
     t.deepEqual(validate(ast.types[2], sayHello), {
+        valid: true,
+        type: '(s -> s -> s)',
+        generics: {},
+        error: null
+    });
+    t.deepEqual(validate(ast.types[3], sayHello), {
         valid: false,
         type: 'Function',
         generics: {},
         error:
-            'Expected (s -> s -> s) in `forall s n. (s -> n -> s) -> (String -> Number -> String) -> (s -> s -> s)` declaration but received (forall a b. a -> b -> a).'
+            'Expected (s -> n -> n) in `forall s n. (s -> n -> s) -> (String -> Number -> String) -> (s -> s -> s) -> (s -> n -> n)` declaration but received (forall a b. a -> b -> a).'
     });
 });
