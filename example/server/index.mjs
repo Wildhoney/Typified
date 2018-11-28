2781fcf43d87bb4698831b370ee26b28f5159642
Wildhoney <adam.timberlake@gmail.com>
2018-11-28 22:58:54 +0000

    Added better example landing page

--- example/server/index.mjs
+++ example/server/index.mjs
@@ -4,7 +4,9 @@ import express from 'express';
 const app = express();
 const example = path.resolve('./example');
 const vendor = path.resolve('./src');
+const media = path.resolve('./media');
 
 app.use('/vendor', express.static(vendor));
+app.use('/media', express.static(media));
 app.use(express.static(example));
 app.listen(process.env.PORT || 3000);
