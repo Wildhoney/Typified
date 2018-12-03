47d3237d92f9218c05ad9fa0e653f12d1b678071
Wildhoney <adam.timberlake@gmail.com>
2018-12-03 22:54:13 +0000

    Reverted example

--- example/js/index.js
+++ example/js/index.js
@@ -1,41 +1,43 @@
 import ꓽ from '/vendor/index.js';
 
-const getName = ꓽ `Promise(String) -> Promise(String)`(async name => {
-    return 'Hello ' + (await name);
-});
+// const getName = ꓽ`Promise(String) -> Promise(String)`(async name => {
+//     return 'Hello ' + (await name);
+// });
 
-console.log(getName(Promise.resolve('Adam')));
+// (async function main() {
+//     console.log(await getName(Promise.resolve('Adam')));
+// })();
 
-// const getHello =
-//     ꓽ `String → Number → String`
-//     ((name, age) => `Hello ${name}! Goodness me you are ${age}!`);
+const getHello =
+    ꓽ `String → Number → String`
+    ((name, age) => `Hello ${name}! Goodness me you are ${age}!`);
 
-// const printAdam =
-//     ꓽ `(String → Number → String) → void`
-//     (helloFn => console.log(helloFn('Adam', 33)));
+const printAdam =
+    ꓽ `(String → Number → String) → void`
+    (helloFn => console.log(helloFn('Adam', 33)));
 
-// printAdam(getHello);
+printAdam(getHello);
 
-// window.t = ꓽ;
-// window.getName = ꓽ`String → String`(name => 'Hello ' + name);
-// console.log('%cWelcome to %cTypified. ', 'color: #434a54', 'color: #434a54; font-weight: bold');
-// console.log(
-//     '%cUse %c`t` %cto set types: %cconst getName = t %c`String → String` %c(name => name)',
-//     'color: #656d78',
-//     'color: deeppink',
-//     'color: #656d78',
-//     'color: deeppink',
-//     'color: green',
-//     'color: deeppink'
-// );
-// console.log(
-//     "%cAnd then call it with different types to test it: %cgetName(%c'Adam'%c) %cand %cgetName(%c33%c)",
-//     'color: #656d78',
-//     'color: deeppink',
-//     'color: green',
-//     'color: deeppink',
-//     'color: #656d78',
-//     'color: deeppink',
-//     'color: green',
-//     'color: deeppink'
-// );
+window.t = ꓽ;
+window.getName = ꓽ`String → Number`(name => 'Hello ' + name);
+console.log('%cWelcome to %cTypified. ', 'color: #434a54', 'color: #434a54; font-weight: bold');
+console.log(
+    '%cUse %c`t` %cto set types: %cconst getName = t %c`String → String` %c(name => name)',
+    'color: #656d78',
+    'color: deeppink',
+    'color: #656d78',
+    'color: deeppink',
+    'color: green',
+    'color: deeppink'
+);
+console.log(
+    "%cAnd then call it with different types to test it: %cgetName(%c'Adam'%c) %cand %cgetName(%c33%c)",
+    'color: #656d78',
+    'color: deeppink',
+    'color: green',
+    'color: deeppink',
+    'color: #656d78',
+    'color: deeppink',
+    'color: green',
+    'color: deeppink'
+);
