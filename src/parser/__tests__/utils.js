0dc6fe7a7035eacf176c8306559320b7ef27e0ee
Wildhoney <adam.timberlake@gmail.com>
2018-12-04 11:45:55 +0000

    Added tests for `determineActualType` function

--- src/parser/__tests__/utils.js
+++ src/parser/__tests__/utils.js
@@ -1,25 +1,25 @@
 import test from 'ava';
-import * as parserUtils from '../utils.js';
+import * as u from '../utils.js';
 
 test('It should be able to determine if a type is a scalar type;', t => {
-    t.deepEqual(parserUtils.parseScalar('Array(String)'), {
+    t.deepEqual(u.parseScalar('Array(String)'), {
         type: 'Array',
         description: 'String'
     });
-    t.deepEqual(parserUtils.parseScalar('Object(name: String)'), {
+    t.deepEqual(u.parseScalar('Object(name: String)'), {
         type: 'Object',
         description: 'name: String'
     });
-    t.deepEqual(parserUtils.parseScalar('(String → Number → Boolean)'), {
+    t.deepEqual(u.parseScalar('(String → Number → Boolean)'), {
         description: 'String → Number → Boolean',
         type: ''
     });
-    t.is(parserUtils.parseScalar('String'), null);
+    t.is(u.parseScalar('String'), null);
 });
 
 test('It should be able to split at the top level only;', t => {
     t.deepEqual(
-        parserUtils.splitTopLevel(
+        u.splitTopLevel(
             'String, Number, Tuple(String, Number), Object(name: Object(first: String, last: String), age: Number), Date, String',
             ','
         ),
--- src/validator/__tests__/utils.js
+++ src/validator/__tests__/utils.js
@@ -1,14 +1,15 @@
 import test from 'ava';
-import * as validatorUtils from '../utils.js';
+import * as u from '../utils.js';
 import { splitTypeDeclaration } from '../../parser/index.js';
+import type from '../../index.js';
 
 test('It should be able to format the type mismatch and length messages;', t => {
     t.is(
-        validatorUtils.formatTypeMismatchMessage(['String'], 'Number', 'Array(String) → Date → String'),
+        u.formatTypeMismatchMessage(['String'], 'Number', 'Array(String) → Date → String'),
         'Expected String in `Array(String) → Date → String` declaration but received Number.'
     );
     t.is(
-        validatorUtils.formatTypeMismatchMessage(
+        u.formatTypeMismatchMessage(
             ['String'],
             'Number',
             'Array(String) → Date → String',
@@ -17,29 +18,48 @@ test('It should be able to format the type mismatch and length messages;', t => 
         'Expected String in `Array(String) → Date → String` declaration but received Number (Array values must be of a single type).'
     );
     t.is(
-        validatorUtils.formatLengthMismatchMessage(3, 2, 'Number → String → Date'),
+        u.formatLengthMismatchMessage(3, 2, 'Number → String → Date'),
         'Expected 3 function parameters but received 2 in `Number → String → Date`.'
     );
 });
 
 test('It should be able to determine the types of values;', t => {
-    t.is(validatorUtils.getType('Adam'), 'String');
-    t.is(validatorUtils.getType([]), 'Array');
-    t.is(validatorUtils.getType({ name: 'Adam' }), 'Object');
-    t.is(validatorUtils.getType(new Date()), 'Date');
-    t.is(validatorUtils.getType(new Promise(() => {})), 'Promise');
-    t.is(validatorUtils.getType(global.BigInt(5)), 'BigInt');
-    t.is(validatorUtils.getType(new Uint16Array()), 'Uint16Array');
-    t.is(validatorUtils.getType(undefined), 'void');
-    t.is(validatorUtils.getType(null), 'void');
-    t.is(validatorUtils.getType(new validatorUtils.Type('Number', splitTypeDeclaration('Number'))), 'Number');
-    t.is(validatorUtils.getType(new validatorUtils.Type('n', splitTypeDeclaration('Number n ⇒ n'))), 'Number');
+    t.is(u.getType('Adam'), 'String');
+    t.is(u.getType([]), 'Array');
+    t.is(u.getType({ name: 'Adam' }), 'Object');
+    t.is(u.getType(new Date()), 'Date');
+    t.is(u.getType(new Promise(() => {})), 'Promise');
+    t.is(u.getType(global.BigInt(5)), 'BigInt');
+    t.is(u.getType(new Uint16Array()), 'Uint16Array');
+    t.is(u.getType(undefined), 'void');
+    t.is(u.getType(null), 'void');
+    t.is(u.getType(new u.Type('Number', splitTypeDeclaration('Number'))), 'Number');
+    t.is(u.getType(new u.Type('n', splitTypeDeclaration('Number n ⇒ n'))), 'Number');
 });
 
 test('It should be able to handle `Type` values;', t => {
-    const type = new validatorUtils.Type('s', splitTypeDeclaration('∀ a. String s, Date d ⇒ s → d'));
-    t.is(validatorUtils.getType(type), 'String');
-    t.is(validatorUtils.getType(type.set('d')), 'Date');
+    const type = new u.Type('s', splitTypeDeclaration('∀ a. String s, Date d ⇒ s → d'));
+    t.is(u.getType(type), 'String');
+    t.is(u.getType(type.set('d')), 'Date');
     t.true(type.set('a').isGeneric());
     t.false(type.set('s').isGeneric());
+});
+
+test('It should be able to determine the actual type of values;', async t => {
+    t.is(u.determineActualType('Adam'), 'String');
+    t.is(u.determineActualType(33), 'Number');
+    t.is(u.determineActualType(['Adam']), 'Array(String)');
+    t.is(u.determineActualType(['Adam', 33]), 'Array(String, Number)');
+    t.is(u.determineActualType({ name: 'Adam' }), 'Object(name: String)');
+    t.is(u.determineActualType({ name: 'Adam', age: 33 }), 'Object(name: String, age: Number)');
+    t.is(
+        u.determineActualType({ name: 'Adam', locations: ['UK', 'RU'] }),
+        'Object(name: String, locations: Array(String))'
+    );
+    t.is(await u.determineActualType(Promise.resolve('Adam')), 'Promise(String)');
+    t.is(u.determineActualType(type`String → String`(name => 'Hello ' + name)), '(String → String)');
+    t.is(
+        u.determineActualType({ names: { first: 'Adam', last: 'Timberlake' }, age: 33, locations: ['UK', 'RU'] }),
+        'Object(names: Object(first: String, last: String), age: Number, locations: Array(String))'
+    );
 });
